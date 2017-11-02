43a6e62fbebb739140f1cd390ae6d881133f6c7f
Rahul <rahulprabhakar0596@gmail.com>
2017-11-02 14:40:01 +0530

    CORS enabled 2

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ let app = angular.module('app',['ngRoute']);
     app.controller("Ctrl",function($scope,$http,$routeParams,$interval){
       $interval(getter,1000);
       function getter(){
-            $http.get('http://theparkingspace.000webhostapp.com/retrieve.php')
+            $http.get('https://theparkingspace.000webhostapp.com/retrieve.php')
                 .success(function(response){
                 $scope.details = response;
             });
